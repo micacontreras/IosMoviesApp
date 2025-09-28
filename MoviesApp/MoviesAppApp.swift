8bac566ee310b091765db0f8b0d5d60792d39f67
Micaela <micaela@gmail.com>
2025-09-28 11:59:50 -0300

    Initial Commit

---
+++ MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Micaela Contreras on 28/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoviesAppTests/MoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppTests.swift
+//  MoviesAppTests
+//
+//  Created by Micaela Contreras on 28/09/2025.
+//
+
+import Testing
+@testable import MoviesApp
+
+struct MoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
